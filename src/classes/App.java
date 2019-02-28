3fc923de2db3f0c09cd7d4c0ceca304da77f28e5
Juri Melnikov <Juri.Melnikov@ivkhk.ee>
2019-02-28 10:05:51 +0200

    Рабочая программа - угадай число

--- src/classes/App.java
+++ src/classes/App.java
@@ -14,21 +14,33 @@ import java.util.Scanner;
  */
 public class App {
     public void run(){
+        System.out.println("-------------------------------");
+        System.out.println("Прогамма задумывает число и просит тебя угдать его ");
+        System.out.println("за указанное число попыток.");
+        System.out.println("-------------------------------");
         Random r = new Random();
         Scanner s = new Scanner(System.in);
         System.out.println("Задай диапазон задумывания чисел:");
-        System.out.println("min = "); 
+        System.out.print("min = "); 
         int min = s.nextInt();
-        System.out.println("max = "); 
+        System.out.print("max = "); 
         int max = s.nextInt();
-        System.out.println("Сколько попыток тебе надо?");
+        System.out.print("Сколько попыток тебе надо? ");
         int attempt = s.nextInt();
         int n = 0;
         int myNumber = r.nextInt(max - min + 1) + min;
         do{
-            System.out.println(
-               "Программа задумала число от "+min+" до "+max+", угадай:"
-            );
+            if(n < 1){
+                System.out.println(
+                    "Программа задумала число от "+min+" до "+max+", угадай:"
+                 );
+                System.out.println("Попытка 1");
+            }else{
+                System.out.println(
+                    "Попытка "+ (n+1)
+                 );
+            }
+            
             int yourNumber = s.nextInt();
             if(myNumber == yourNumber){
                 System.out.println("Ты выиграл!");
